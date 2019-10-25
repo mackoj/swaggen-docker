254f4f3fd5423d88d3bf3c6a1d7b09a9a5d2795d
Jeffrey MACKO <github.jm@macko.fr>
2019-10-25 16:01:25 +0200

    Make it work for solocal

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-declare SWAGGEN_VERSION="master"
+declare SWAGGEN_VERSION="customPJService"
 declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun Ayachit <m0t0rbr3th@gmail.com>"
-declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
+declare SWAGGEN_REPO="https://github.com/mackoj/SwagGen"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="master-ccd0ec6-slim"
-declare DOCKER_VERSION_ALIAS="latest-unstable"
+declare DOCKER_VERSION="master-c01f16d-slim"
+declare DOCKER_VERSION_ALIAS="latest-solocal-unstable"
 
