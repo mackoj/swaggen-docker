bbe75d3915193a4d5c9791fc10f0197a614fa96c
Jeffrey MACKO <github.jm@macko.fr>
2019-10-25 16:27:18 +0200

    generator ok ?

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-declare SWAGGEN_VERSION="customPJService"
+declare SWAGGEN_VERSION="CustomTemplates"
 declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun Ayachit <m0t0rbr3th@gmail.com>"
 declare SWAGGEN_REPO="https://github.com/mackoj/SwagGen"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="customPJService-slim"
+declare DOCKER_VERSION="CustomTemplates-slim"
 declare DOCKER_VERSION_ALIAS="latest-solocal-unstable"
 
