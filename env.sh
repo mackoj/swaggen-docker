7d6f0aa380bf4465f87f3f53a0a5addca0930bc4
Jeffrey MACKO <github.jm@macko.fr>
2019-10-25 12:33:44 +0200

    Support de master de swaggen

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 # Set version
-declare SWAGGEN_VERSION="$(cat VERSION)"
+declare SWAGGEN_VERSION="master"
 declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun Ayachit <m0t0rbr3th@gmail.com>"
 declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="4.2.0-slim"
+declare DOCKER_VERSION="master-slim"
 
