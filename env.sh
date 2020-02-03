8bf39edefd30ff9cef0f1fd0e9fefff96936a374
Jeffrey Macko <github.jm@macko.fr>
2020-02-03 23:48:29 +0100

    Add testing unstable Swift 5.2

--- env.sh
+++ env.sh
@@ -5,10 +5,10 @@ declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun A
 declare SWAGGEN_REPO="https://github.com/${REPO}"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="${LAST_SWAGGEN_VERSION}-slim"
-declare DOCKER_VERSION_ALIAS="latest"
+declare DOCKER_VERSION="${LAST_SWAGGEN_VERSION}-slim-5.2-2020-02-01"
+declare DOCKER_VERSION_ALIAS="-5.2-2020-02-01"
 
-./getLastSwiftTag.swift "apple/swift" "-RELEASE" NEW_SWIFT_VERSION
+./getLastSwiftTag.swift "apple/swift" "-2020-02-01-a" NEW_SWIFT_VERSION
 
 LAST_SWIFT_VERSION="$(cat NEW_SWIFT_VERSION)"
 OLD_LAST_SWAGGEN_VERSION="$(cat VERSION)"
